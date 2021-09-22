c0db403851fbc34245677cc1506850c5c89f91cb
Cylindrik <gabriel_ferr@hotmail.com>
2021-09-22 11:23:51 -0400

    pinkman

---
+++ joguin/Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+
+    public int totalScore;
+    public Text scoreText;
+
+    public static GameController instance;
+
+    public GameObject gameOver;
+
+       
+    void Start()
+    {
+        instance = this;
+
+    }
+
+    public void UpdateScoreText()
+    {
+        scoreText.text = totalScore.ToString();
+    }
+
+    public void ShowGameOver()
+    {
+        gameOver.SetActive(true);
+    }
+
+    public void RestartGame(string lvlName)
+    {
+        SceneManager.LoadScene(lvlName);
+    }
+
+}
---
+++ joguin/Assets/Scripts/Player.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed;
+    public float JumpForce;
+    private Rigidbody2D rig;
+
+    public bool isJumping;
+    public bool doubleJump;
+    private Animator anim;
+
+   
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        /*Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);*/
+        float movement = Input.GetAxis("Horizontal");
+
+        /*transform.position += movement * Time.deltaTime * Speed;*/
+        rig.velocity = new Vector2(movement * Speed, rig.velocity.y );
+        
+        if(movement > 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f,0f,0f);
+        }
+        
+        if(movement < 0f)
+        {
+            anim.SetBool("walk", true);
+            transform.eulerAngles = new Vector3(0f,180f,0f);
+        }
+        
+        if(movement == 0f)
+        {
+            anim.SetBool("walk", false);
+        }
+    }
+
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {   
+            if (!isJumping)
+            {
+                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                doubleJump = true;
+                anim.SetBool("jump", true);
+            }
+            else
+            {
+                if(doubleJump)
+                {
+                    rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                    doubleJump = false;
+                }
+            }
+            
+            
+
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            isJumping = false;
+            anim.SetBool("jump", false);
+        }
+
+         if(collision.gameObject.tag == "spike")
+        {
+           GameController.instance.ShowGameOver();
+           Destroy(gameObject);
+        }
+
+        if(collision.gameObject.tag == "Saw")
+        {
+           GameController.instance.ShowGameOver();
+           Destroy(gameObject);
+        }
+
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        
+        if(collision.gameObject.layer == 8)
+        {
+             isJumping = true;
+        }
+    }
+
+}
